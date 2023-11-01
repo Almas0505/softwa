4fe94b9b39672d9b4b818958e02d8ffed400c522
Almas0505 <149063213+Almas0505@users.noreply.github.com>
2023-11-02 02:14:18 +0600

    WeatherApp.java

--- src/main/java/org/example/WeatherApp.java
+++ src/main/java/org/example/WeatherApp.java
@@ -6,8 +6,111 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class WeatherApp {
+class WeatherData {
+    private int temperature;
+    private String description;
+    private int windChill;
+    private double pressure;
+    private int humidity;
+    private double windSpeed;
+    private int windDirection;
+    private int cloudiness;
+    private double visibility;
 
+    public WeatherData() {}
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = (int) (temperature - 273.15);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWindChill() {
+        return windChill;
+    }
+
+    public void setWindChill(int windChill) {
+        this.windChill = windChill;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public int getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(int windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public int getCloudiness() {
+        return cloudiness;
+    }
+
+    public void setCloudiness(int cloudiness) {
+        this.cloudiness = cloudiness;
+    }
+
+    public double getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(double visibility) {
+        this.visibility = visibility;
+    }
+}
+
+class WeatherDataAdapter {
+    public static WeatherData adaptFromJson(JSONObject jsonResponse) {
+        WeatherData weatherData = new WeatherData();
+
+        weatherData.setTemperature(jsonResponse.getJSONObject("main").getInt("temp"));
+        weatherData.setDescription(jsonResponse.getJSONArray("weather").getJSONObject(0).getString("description"));
+        weatherData.setWindChill(0);
+        weatherData.setPressure(jsonResponse.getJSONObject("main").getDouble("pressure"));
+        weatherData.setHumidity(jsonResponse.getJSONObject("main").getInt("humidity"));
+        weatherData.setWindSpeed(jsonResponse.getJSONObject("wind").getDouble("speed"));
+        weatherData.setWindDirection(jsonResponse.getJSONObject("wind").getInt("deg"));
+        weatherData.setCloudiness(jsonResponse.getJSONObject("clouds").getInt("all"));
+        weatherData.setVisibility(jsonResponse.getInt("visibility") / 1000.0);
+
+        return weatherData;
+    }
+}
+
+public class WeatherApp {
     private static final String API_KEY = "31314b4fa0621986d2a0d8361009365e";
 
     public static void main(String[] args) {
@@ -33,40 +136,18 @@ public class WeatherApp {
                 }
                 inputReader.close();
 
-
                 JSONObject jsonResponse = new JSONObject(response.toString());
-                JSONObject main = jsonResponse.getJSONObject("main");
-                JSONObject wind = jsonResponse.getJSONObject("wind");
-                JSONObject clouds = jsonResponse.getJSONObject("clouds");
+                WeatherData weatherData = WeatherDataAdapter.adaptFromJson(jsonResponse);
 
-                double temperatureKelvin = main.getDouble("temp");
-
-
-                int temperatureCelsius = (int) (temperatureKelvin - 273.15);
-                double pressure = main.getDouble("pressure");
-                int humidity = main.getInt("humidity");
-                double windSpeedMs = wind.getDouble("speed");
-                double windSpeedKmh = windSpeedMs * 3.6;
-                int windDirection = wind.getInt("deg");
-                int cloudiness = clouds.getInt("all");
-                int visibilityMeters = jsonResponse.getInt("visibility");
-                double visibilityKm = visibilityMeters / 1000.0;
-                String description = jsonResponse.getJSONArray("weather").getJSONObject(0).getString("description");
-                double windChillIndex = 13.12 + 0.6215 * temperatureCelsius - 11.37 * Math.pow(windSpeedMs, 0.16) + 0.3965 * temperatureCelsius * Math.pow(windSpeedMs, 0.16);
-
-
-                int windChillTemperature = (int) windChillIndex;
-
-                System.out.println();
-                System.out.println("Temperature: " + temperatureCelsius + "°C");
-                System.out.println("Description: " + description);
-                System.out.println("Wind Chill: " + windChillTemperature + "°C");
-                System.out.println("Pressure: " + pressure + " hPa");
-                System.out.println("Humidity: " + humidity + "%");
-                System.out.println("Wind Speed: " + windSpeedKmh + " km/h");
-                System.out.println("Wind Direction: " + windDirection + "°");
-                System.out.println("Cloudiness: " + cloudiness + "%");
-                System.out.println("Visibility: " + visibilityKm + " km");
+                System.out.println("Temperature: " + weatherData.getTemperature() + "°C");
+                System.out.println("Description: " + weatherData.getDescription());
+                System.out.println("Wind Chill: " + weatherData.getWindChill() + "°C");
+                System.out.println("Pressure: " + weatherData.getPressure() + " hPa");
+                System.out.println("Humidity: " + weatherData.getHumidity() + "%");
+                System.out.println("Wind Speed: " + weatherData.getWindSpeed() + " km/h");
+                System.out.println("Wind Direction: " + weatherData.getWindDirection() + "°");
+                System.out.println("Cloudiness: " + weatherData.getCloudiness() + "%");
+                System.out.println("Visibility: " + weatherData.getVisibility() + " km");
 
             } else {
                 System.out.println("Failed to retrieve weather data for the city: " + city);
@@ -76,3 +157,4 @@ public class WeatherApp {
         }
     }
 }
+
